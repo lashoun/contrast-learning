77278314a8335bc6221372e0cfea990e4b7e5577
lashoun <ashikaga@tuta.io>
2019-04-29 19:25:04 +0200

    added per dimension outlier detection

--- first_draft.py
+++ first_draft.py
@@ -26,8 +26,11 @@
 # - Inverse law when seeing duplicates
 # - Implement context
 # - Compare stdevs and distances
-# - ✔ Clustering validity checking methods
+# - Clustering validity checking methods (Note: not necessarily relevant
+#   though)
 # - Recollection: random updates vs complete updates?
+# - Hub clusters
+# - Forgetting data
 #
 # ## Not to do
 # - Implement a tree-like structure for clusters (not relevant if we update
@@ -60,8 +63,8 @@ np.random.seed(1)
 
 # %%
 NB_FEATURES = 5
-NB_GROUPS = 10
-N = 200
+NB_GROUPS = 3
+N = 20
 DOMAIN_LENGTH = 200
 DEV_MAX = 20
 
@@ -130,55 +133,48 @@ def generate_dataset(nb_groups=NB_GROUPS, n=N, nb_features=NB_FEATURES,
     np.save(DATASET_PATH + DATASET_NAME + '_clusters_true.npy', clusters_true)
 
 
-def distance(p, q, ord=2):
-    return(np.linalg.norm(p-q, ord))
-
-
-def get_outlier_ratio(p, cluster_points, method='maha', alpha=0.95):
-    """
-    The 'method' argument can be either of the following:
-    - 'maha': Mahalanobis distance, generalization of the distance to
-              the mean in terms of standard deviations for multivariate data
-    - 'cook':
-    - 'mmcd':
-    """
-    threshold = chi2.isf(1-alpha, p.shape[1])
-    if method == 'cook':
-        raise NotImplementedError("'cook' method not yet implemented")
-    elif method == 'mmcd':
-        raise NotImplementedError("'mmcd' method not yet implemented")
-    else:
-        if method != 'maha':
-            print("Unknown method, using Mahalanobis distance")
-        sigma = np.cov(cluster_points)
-        mu = np.mean(cluster_points, axis=0)
-        d = mahalanobis(p, mu, np.linalg.inv(sigma))
-        ratio = d / threshold
-        return ratio
-
-
 # %% [markdown]
 # ## The agent
 
 # %%
 class ContrastAgent(object):
-    def __init__(self, sensitivenesses=[1, 10, 0.1], shuffleToggle=False,
+    def __init__(self,
+                 method_find='byhand',
+                 method_multivariate='maha',
+                 method_univariate='mad',
+                 alpha=0.95,
+                 ord=2,
+                 sensitivenesses=[1, 10, 0.1],
+                 shuffleToggle=False,
                  verbose=False):
 
         # --- user-defined parameters ---
-        self.sensitiveness_find_cluster = sensitivenesses[0]
-        # if a point is alone with a radius of
-        # sensitiveness_find_cluster * stdist, create a new cluster
-        self.sensitiveness_inf_dims = sensitivenesses[1]
-        # sensitiveness to determine if a dimension is too variable to be
-        # relevant for the cluster
-        self.sensitiveness_zero_dims = sensitivenesses[2]
-        # sensitiveness to determine if a dimension is too concentrated to be
-        # relevant for the cluster
+
+        self.method_find = method_find
+        self.method_multivariate = method_multivariate
+        self.method_univariate = method_univariate
+        self.ord = ord
         self.shuffleToggle = shuffleToggle
         self.verbose = verbose
 
-        # --- other parameters ---
+        # # --- 'byhand' method specific paramters ---
+        self.sensitiveness_find_cluster = sensitivenesses[0]
+        # if a point is alone with a radius of
+        # sensitiveness_find_cluster * stdist, create a new cluster
+
+        self.sensitiveness_inf_dims = sensitivenesses[1]
+        # sensitiveness to determine if a dimension is too variable to be
+        # relevant for the cluster
+
+        self.sensitiveness_zero_dims = sensitivenesses[2]
+        # sensitiveness to determine if a dimension is too concentrated to be
+        # relevant for the cluster
+
+        # # --- 'maha' method specific parameters ---
+        self.alpha = alpha
+
+        # --- self-initialized parameters ---
+        self.allZeros = True
         self.clusters = np.array([])
         # clusters[i] == j means that point i belongs to cluster j
         self.cluster_sizes = []
@@ -196,9 +192,12 @@ class ContrastAgent(object):
             self.new_cluster(0)
             self.one_more_seen()
         for i, p in enumerate(self.data[self.nb_seen:], start=self.nb_seen):
-            allZeros = self.find_cluster(i, p, until=i)
-            if not allZeros:
+            self.find_cluster(i, p, until=i)
+            if not self.allZeros:
                 self.one_more_seen()
+
+    def distance(self, p, q, ord=2):
+        return(np.linalg.norm(p-q, self.ord))
 
     def feed_data(self, d):
         """Adds data to the agent's memory"""
@@ -218,30 +217,106 @@ class ContrastAgent(object):
             self.clusters = new_clusters_all
 
     def find_cluster(self, i, p, until=None, recollection=False):
-        distances = np.array([distance(p, q) for q in self.data[:until]])
-        allZeros = np.all(distances == 0)
-        dist_min = np.min(np.trim_zeros(distances)) if not allZeros else 0
-        closest = np.argmin(distances)
-        if dist_min > self.sensitiveness_find_cluster * self.stdist and \
-                0 not in distances:
-            if self.verbose:
-                print("{} -> new cluster".format(i))
-            if self.clusters[i] == -1 or \
-                    not self.cluster_sizes[self.clusters[i]] == 1:
-                # if p not already seen or is not already alone
+        """
+        The 'method' argument can be on the following:
+        'byhand', 'byhand-naive', 'maha', 'cook', 'mmcd'.
+        """
+        method = self.method_find
+        if method in ['maha', 'cook', 'mmcd']:
+            global_ratio = self.get_outlier_ratio(p, self.data)
+            if global_ratio > 1:
                 self.new_cluster(i)
+                if self.verbose:
+                    print("{} -> new cluster".format(i))
+            else:
+                cluster_ratios = []
+                for j, size in enumerate(self.cluster_sizes):
+                    if size != 0:
+                        cluster_points = self.get_cluster_points(j)
+                        ratio = self.get_outlier_ratio(p, cluster_points)
+                        cluster_ratios.append([j, ratio])
+                cluster_ratios = np.array(cluster_ratios)
+                bci = np.argmin(cluster_ratios[:, 1])
+                bc = cluster_ratios[bci, 0]
+                self.clusters[i] = bc
+                if self.verbose:
+                    print(cluster_ratios)
+                    print("Best cluster is {} with a ratio of {}".format(
+                        bc, cluster_ratios[bci, 1]))
+                    print("{} -> cluster of {}".format(i, bc))
+        elif method == 'byhand-naive':
+            distances = np.array([self.distance(p, q, ord) for q
+                                  in self.data[:until]])
+            self.allZeros = self.allZeros and np.all(distances == 0)
+            dist_min = np.min(np.trim_zeros(distances)) if not self.allZeros \
+                else 0
+            closest = np.argmin(distances)
+            if dist_min > self.sensitiveness_find_cluster * self.stdist and \
+                    0 not in distances:
+                if self.verbose:
+                    print("{} -> new cluster".format(i))
+                if self.clusters[i] == -1 or \
+                        not self.cluster_sizes[self.clusters[i]] == 1:
+                    # if p not already seen or is not already alone
+                    self.new_cluster(i)
+            else:
+                if self.verbose:
+                    print("{} -> cluster of {}".format(i, closest))
+                self.clusters[i] = self.clusters[closest]
+                if not self.allZeros:
+                    self.update_stdists_per_cluster(self.clusters[i], p)
+                self.cluster_sizes[self.clusters[i]] += 1
+            if not recollection and not self.allZeros:
+                self.update_stdist(self.nb_seen, dist_min)
         else:
+            p_i = i  # Variable i will be replaced afterwards, watch out!
+            if method != 'byhand':
+                print("find_cluster: unknown method, using 'byhand'")
+            cluster_indexes = []
+            cluster_dims = []
+            cluster_good_ratios_sum = []
+            cluster_bad_ratios_sum = []
+            for i in self.clusters:
+                if i != -1:
+                    relevant_dims = 0
+                    good_ratios_sum = 0
+                    bad_ratios_sum = 0
+                    ratios = self.get_outlier_ratio_per_dimension(i, p)
+                    for r in ratios:
+                        if r < 1:
+                            relevant_dims += 1
+                            good_ratios_sum += r
+                        else:
+                            bad_ratios_sum += r
+                    cluster_indexes.append(i)
+                    cluster_dims.append(relevant_dims)
+                    cluster_good_ratios_sum.append(good_ratios_sum)
+                    cluster_bad_ratios_sum.append(bad_ratios_sum)
+            ind = np.lexsort((cluster_indexes,
+                              cluster_bad_ratios_sum,
+                              cluster_good_ratios_sum,
+                              -np.array(cluster_dims)))
+            closest = cluster_indexes[ind[0]]
             if self.verbose:
-                print("{} -> cluster of {}".format(i, closest))
-            self.clusters[i] = self.clusters[closest]
-            if not allZeros:
-                self.update_stdists_per_cluster(self.clusters[i], p)
-            self.cluster_sizes[self.clusters[i]] += 1
-        if not recollection and not allZeros:
-            self.update_stdist(self.nb_seen, dist_min)
-        return allZeros
+                print([(cluster_dims[i],
+                        cluster_good_ratios_sum[i],
+                        cluster_bad_ratios_sum[i],
+                        cluster_indexes[i])
+                       for i in ind[0: min(len(ind), 3)]])
+            if cluster_dims[ind[0]] == 0 or \
+                    self.nb_clusters == 1:
+                if self.clusters[p_i] == -1 or \
+                        not self.cluster_sizes[self.clusters[p_i]] == 1:
+                    if self.verbose:
+                        print("{} -> new cluster".format(p_i))
+                    self.new_cluster(p_i)
+            else:
+                if self.verbose:
+                    print("{} -> cluster of {}".format(p_i, closest))
+                self.clusters[p_i] = self.clusters[closest]
+                self.cluster_sizes[self.clusters[p_i]] += 1
 
-    def get_cluster_dimensions(self, i):
+    def get_cluster_relevant_dimensions(self, i):
         """ return (relevant_dims, inf_dims, zero_dims) """
         stdist_i = self.stdists_per_cluster[i]
         cluster_points = self.get_cluster_points(i)
@@ -257,7 +332,83 @@ class ContrastAgent(object):
         return relevant_dims, inf_dims, zero_dims
 
     def get_cluster_points(self, i):
-        return self.data[np.argwhere(self.clusters == i)]
+        return self.data[np.nonzero(self.clusters == i)]
+
+    def get_nb_obs_per_dimension(self, i):
+        """ returns the number of distinct observations per dimension """
+        cluster_points = self.get_cluster_points(i)
+        d = cluster_points.shape[1]
+        nb_obs = np.zeros(d)
+        for j in range(d):
+            nb_obs[j] = len(np.unique(cluster_points[:, j]))
+        return nb_obs
+
+    def get_outlier_ratio_per_dimension(self, i, p):
+        cluster_points = self.get_cluster_points(i)
+        d = cluster_points.shape[1]
+        if d == 1:
+            np.hstack(cluster_points, np.mean(cluster_points,
+                                              np.reshape(p, (-1, 1))))
+            # arbitrary choice: we add the mean to get a more relevant result
+        ratios = np.zeros(d)
+        for j in range(d):
+            ratios[j] = self.get_outlier_ratio_univariate(p[j],
+                                                          cluster_points[:, j])
+        return ratios
+
+    def get_outlier_ratio(self, p, cluster_points_without_p):
+        """
+        The 'method' argument can be either of the following:
+        - 'maha': Mahalanobis distance, generalization of the distance to
+                  the mean in terms of standard deviations for
+                  multivariate data
+        - 'cook': TODO
+        - 'mmcd': TODO
+        """
+        eps = np.mean(cluster_points_without_p[0]) * 1e-6
+        # used to avoid singular matrices
+        cluster_points = np.vstack([p, cluster_points_without_p])
+        threshold = chi2.isf(1-self.alpha, len(p))
+        method = self.method_multivariate
+        if method == 'cook':
+            raise NotImplementedError("'cook' method not yet implemented")
+        elif method == 'mmcd':
+            raise NotImplementedError("'mmcd' method not yet implemented")
+        else:
+            if method != 'maha':
+                print("Unknown method, using Mahalanobis distance")
+            sigma = np.cov(cluster_points, rowvar=False) + eps * np.eye(len(p))
+            mu = np.mean(cluster_points, axis=0)
+            d = mahalanobis(p, mu, np.linalg.inv(sigma))
+            ratio = d / threshold
+            return ratio
+
+    def get_outlier_ratio_univariate(self, p, cluster_points_without_p):
+        """
+        The 'method' argument can be one of the following:
+        - 'mean': mean +- three stdevs
+        - 'mad': median absolute deviation, more robust to detect outliers
+
+        If the value returned is > 1, consider p an outlier.
+        """
+        B = 1.4826
+        threshold = 3
+        method = self.method_univariate
+        cluster_points = np.hstack([cluster_points_without_p, [p]])
+        if method == 'mean':
+            raise NotImplementedError("'mean' method not yet implemented")
+        else:
+            if method != 'mad':
+                print("Unknown method_univariate, using median "
+                      "absolute deviation")
+            m = np.median(cluster_points)
+            mad = B * np.median(np.abs(cluster_points - m))
+            if np.abs(p-m) == 0:
+                return 0
+            elif mad == 0:
+                return np.inf
+            else:
+                return (np.abs(p-m)/(threshold*mad))
 
     def new_cluster(self, p_index):
         self.clusters[p_index] = self.nb_clusters
@@ -319,7 +470,8 @@ class ContrastAgent(object):
         else:
             cluster_points_rd = self.get_cluster_points(i)[:, zero_dims]
             p_rd = p[zero_dims]
-        array_dists = np.array([distance(p_rd, q) for q in cluster_points_rd])
+        array_dists = np.array([self.distance(p_rd, q) for q in
+                                cluster_points_rd])
 #         print(relevant_dims, inf_dims, zero_dims)
 #         print(array_dists)
         dist_p = np.min(array_dists)
@@ -361,12 +513,16 @@ true_data_plot = sns.pairplot(df, kind="scatter", hue='true cluster',
                               vars=columns[:-1])
 true_data_plot.savefig(DATASET_PATH + DATASET_NAME + '_true.png')
 
+# %% [markdown]
+# ## Clusterize
+
 # %%
-ca = ContrastAgent(sensitivenesses=[1.25, 10, 0.1], shuffleToggle=True,
-                   verbose=False)
+ca = ContrastAgent(method_find='byhand', sensitivenesses=[1.25, 10, 0.1],
+                   shuffleToggle=True, verbose=True)
 ca.feed_data(data)
 ca.clusterize_online()
-print("Final stdist: {}".format(ca.stdist))
+if ca.method_find == 'byhand-naive':
+    print("Final stdist: {}".format(ca.stdist))
 print("All points in a cluster? {}".format(-1 not in ca.clusters))
 ca.print_clusters()
 
@@ -383,7 +539,7 @@ def print_metrics():
         clusters_true2 = np.array([ids[label] for label in clusters_true])
         if ca.shuffleToggle:
             clusters_true2 = clusters_true2[ca.permutation]
-        print("Metrics involving ground truth")
+        print("--- Metrics involving ground truth--- ")
         print("Adjusted Rand Index: {}".format(
             metrics.adjusted_rand_score(clusters_true2, ca.clusters)))
         print("Adjusted Mutual Information score: {}".format(
@@ -398,7 +554,7 @@ def print_metrics():
             metrics.fowlkes_mallows_score(clusters_true2, ca.clusters)))
         print()
 
-    print("Metrics not involving ground truth")
+    print("--- Metrics not involving ground truth ---")
     if clusters_true is not None:
         print("Silhouette score (original): {}".format(
             metrics.silhouette_score(data, clusters_true)))
@@ -431,5 +587,3 @@ print_metrics()
 # %%
 ca.update_clusters()
 print_metrics()
-
-# %%
